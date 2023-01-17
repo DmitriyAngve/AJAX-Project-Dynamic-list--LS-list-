6f49f5ee9aeee1fb5c81a1d7aa80cbc998a43a4f
Dmitriy Angve <kirirch129@gmail.com>
2023-01-17 11:55:39 +0300

    #

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const output = document.querySelector(".output");
+const btn1 = document.createElement("button");
+btn1.textContent = "Reload";
+btn1.addEventListener("click", reloader);
+document.body.append(btn1);
+btn1.classList.add("btn");
 
-console.log(output);
+const input1 = document.createElement("input");
+const input2 = document.createElement("input");
+const input3 = document.createElement("input");
+const input4 = document.createElement("input");
+const input5 = document.createElement("input");
+const input6 = document.createElement("input");
+const btn2 = document.createElement("button");
+const div1 = document.createElement("div");
+appender(input1);
+appender(input2);
+appender(input3);
+appender(input4);
+appender(input5);
+appender(input6);
+appender(btn2);
+
+btn2.textContent = "Add to Army List";
+input1.setAttribute("placeholder", "Fraction");
+input2.setAttribute("placeholder", "Leader Name");
+input3.setAttribute("placeholder", "Unit");
+input4.setAttribute("placeholder", "Status");
+input5.setAttribute("placeholder", "Hitpoints");
+input6.setAttribute("placeholder", "Taken Damage");
+
+document.body.append(div1);
+document.body.append(bnt1);
+btn2.addEventListener("click", addToList);
+
+function appender(element) {
+  div1.append(element);
+}
+
 const url = "list.json";
 let myList = [];
 let localData = localStorage.get;
@@ -12,15 +48,35 @@ window.addEventListener("DOMContentLoaded", () => {
     myList = JSON.parse(localStorage.getItem("myList"));
     maker();
   } else {
-    fetch(url)
-      .then((rep) => rep.json())
-      .then((data) => {
-        myList = data;
-        maker();
-        saveToStorage();
-      });
+    reloader();
   }
 });
+
+function addToList() {
+  const myObj = {
+    fraction: input1.value,
+    leader: input2.value,
+    unit: input3.value,
+    status: input4.value,
+    hitpoints: input5.value,
+    damage: input6.value,
+  };
+
+  const val = myList.length;
+  myList.push(myObj);
+  saveToStorage();
+  makeList(myObj, val);
+}
+
+function reloader() {
+  fetch(url)
+    .then((rep) => rep.json())
+    .then((data) => {
+      myList = data;
+      maker();
+      saveToStorage();
+    });
+}
 
 function maker() {
   output.innerHTML = "";
@@ -52,9 +108,9 @@ function makeList(item, index) {
     } else {
       myList[index].status = "DEAD";
     }
-    console.log(myList);
+    // console.log(myList);
 
-    localStorage.setItem("myList", JSON.stringify(myList));
+    // localStorage.setItem("myList", JSON.stringify(myList));
 
     div.innerHTML = `${item.fraction} leaded with "${
       item.leader
